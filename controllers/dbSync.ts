07db1f42129f7c48f823985a922b8ea8d637f89a
Jose Fernandez <jotaj1019@gmail.com>
2023-11-02 11:44:01 -0600

    twenty seventh commit: created a new controller 'dbSync', and using it in api/dbSync

---
+++ controllers/dbSync.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { airtableBase } from "lib/airtable";
+import { productsIndex } from "lib/algolia";
+
+export async function SyncDatabase(res: NextApiResponse) {
+    airtableBase("tbl5llKcrP2mcRamZ")
+        .select({
+            pageSize: 10,
+        })
+        .eachPage(
+            async function (records, fetchNextPage) {
+                const objects = records.map((r) => {
+                    return {
+                        objectID: r.id,
+                        ...r.fields,
+                    };
+                });
+                await productsIndex.saveObjects(objects);
+                fetchNextPage();
+            },
+            function done(err) {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send("Database Sync failed");
+                    return;
+                }
+                res.status(200).send("Database Sync done");
+            }
+        );
+}
--- pages/api/dbSync/index.ts
+++ pages/api/dbSync/index.ts
@@ -1,30 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { airtableBase } from "lib/airtable";
-import { productsIndex } from "lib/algolia";
+import { SyncDatabase } from "controllers/dbSync";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-    airtableBase("tbl5llKcrP2mcRamZ")
-        .select({
-            pageSize: 10,
-        })
-        .eachPage(
-            async function (records, fetchNextPage) {
-                const objects = records.map((r) => {
-                    return {
-                        objectID: r.id,
-                        ...r.fields,
-                    };
-                });
-                await productsIndex.saveObjects(objects);
-                fetchNextPage();
-            },
-            function done(err) {
-                if (err) {
-                    console.error(err);
-                    res.status(500).send("Database Sync failed");
-                    return;
-                }
-                res.status(200).send("Database Sync done");
-            }
-        );
+    try {
+        await SyncDatabase(res);
+    } catch (e) {
+        res.status(500).send({
+            message: "An error occurred",
+            error: e,
+        });
+    }
 }
