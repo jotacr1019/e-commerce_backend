177f63bd49b2a89cd39e7768d38d4d97c6bb09ee
Jose Fernandez <jotaj1019@gmail.com>
2023-12-16 19:29:43 -0600

    changed the name of the index

--- lib/algolia.ts
+++ lib/algolia.ts
@@ -4,4 +4,4 @@ const algoliaAppId = process.env.algolia_APP_ID;
 const algoliaAdminApiKey = process.env.algolia_admin_API_KEY;
 
 const client = algoliasearch(algoliaAppId, algoliaAdminApiKey);
-export const productsIndex = client.initIndex("products");
+export const productsIndex = client.initIndex("productsECommerce");
